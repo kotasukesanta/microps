acbcd14e9373679d6efbb864daabc054ea2f0d11
Hiroshi Matsumoto <5595351+kotasukesanta@users.noreply.github.com>
2023-08-30 08:39:46 +0900

    step6

--- ip.c
+++ ip.c
@@ -7,6 +7,7 @@
 #include "net.h"
 #include "ip.h"
 
+// IPヘッダ構造体
 struct ip_hdr {
     uint8_t vhl;        // Version(4bit) + IHL(4bit)
     uint8_t tos;        // Type Of Service(8bit)
@@ -24,6 +25,7 @@ struct ip_hdr {
 const ip_addr_t IP_ADDR_ANY       = 0x00000000; /* 0.0.0.0 */
 const ip_addr_t IP_ADDR_BROADCAST = 0xffffffff; /* 255.255.255.255 */
 
+// IPアドレス（文字列）をネットワークバイトオーダーのバイナリ値に変換
 int
 ip_addr_pton(const char *p, ip_addr_t *n)
 {
@@ -49,6 +51,7 @@ ip_addr_pton(const char *p, ip_addr_t *n)
     return 0;
 }
 
+// IPアドレス（ネットワークバイトオーダーのバイナリ値）を文字列に変換
 char *
 ip_addr_ntop(ip_addr_t n, char *p, size_t size)
 {
@@ -59,15 +62,78 @@ ip_addr_ntop(ip_addr_t n, char *p, size_t size)
     return p;
 }
 
+// IPデータグラムをダンプします。
 static void
 ip_dump(const uint8_t *data, size_t len)
 {
+    struct ip_hdr *hdr;
+    uint8_t v, hl, hlen;
+    uint16_t total, offset;
+    char addr[IP_ADDR_STR_LEN];
+
+    flockfile(stderr);
+    hdr = (struct ip_hdr *)data;
+    v = (hdr->vhl & 0xf0) >> 4;
+    hl = hdr->vhl & 0x0f;
+    hlen = hl << 2;
+    fprintf(stderr, "        vhl: 0x%02x [v: %u, hl: %u (%u)]\n", hdr->vhl, v, hl, hlen);
+    fprintf(stderr, "        tos: 0x%02x\n", hdr->tos);
+    total = ntoh16(hdr->total);
+    fprintf(stderr, "      total: %u (payload: %u)\n", total, total - hlen);
+    fprintf(stderr, "         id: %u\n", ntoh16(hdr->id));
+    offset = ntoh16(hdr->offset);
+    fprintf(stderr, "     offset: 0x%04x [flags=%x, offset=%u]\n", offset, (offset & 0xe000) >> 13, offset & 0x1fff);
+    fprintf(stderr, "        ttl: %u\n", hdr->ttl);
+    fprintf(stderr, "   protocol: %u\n", hdr->protocol);
+    fprintf(stderr, "        sum: 0x%04x\n", ntoh16(hdr->sum));
+    fprintf(stderr, "        src: %s\n", ip_addr_ntop(hdr->src, addr, sizeof(addr)));
+    fprintf(stderr, "        dst: %s\n", ip_addr_ntop(hdr->dst, addr, sizeof(addr)));
+#ifdef HEXDUMP
+    hexdump(stderr, data, len);
+#endif
+    funlockfile(stderr);
 }
 
 // プロトコルの受信キューからデータを受領します。
 static void
 ip_input(const uint8_t *data, size_t len, struct net_device *dev)
 {
+    struct ip_hdr *hdr;
+    uint8_t v;
+    uint16_t hlen, total, offset;
+    uint16_t sum;
+
+    if (len < IP_HDR_SIZE_MIN) {
+        errorf("too short");
+        return;
+    }
+    hdr = (struct ip_hdr *)data;
+
+    // Exercise 6-1: IPデータグラムの検証
+    //  (1) バージョン
+    v = (hdr->vhl & 0xf0) >> 4;
+    if (IP_VERSION_IPV4 != v) {
+        errorf("does not match IPv4");
+        return;
+    }
+    //  (2) ヘッダ長
+    hlen = (hdr->vhl & 0x0f) << 2;
+    //  (3) トータル長
+    total = ntoh16(hdr->total);
+    //  (4) チェックサム
+    sum = cksum16((uint16_t *)data, hlen, 0);
+    if (0x0000 != sum) {
+        errorf("does not match checksum");
+        return;
+    }
+
+    offset = ntoh16(hdr->offset);
+    if (offset & 0x2000 || offset & 0x1fff) {
+        errorf("fragments does not support");
+        return;
+    }
+    debugf("dev=%s, protocol=%u, total=%u", dev->name, hdr->protocol, total);
+    ip_dump(data, total);
 }
 
 // プロトコルを初期化します。
---
+++ test/step6.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+#include <signal.h>
+#include <unistd.h>
+
+#include "util.h"
+#include "net.h"
+
+#include "driver/loopback.h"
+
+#include "test.h"
+
+static volatile sig_atomic_t terminate;
+
+static void
+on_signal(int s)
+{
+    (void)s;
+    terminate = 1;
+}
+
+int
+main(int argc, char *argv[])
+{
+    struct net_device *dev;
+
+    // シグナルハンドラを設定します。
+    signal(SIGINT, on_signal);
+    // プロトコルスタックを初期化します。
+    if (net_init() == -1) {
+        errorf("net_init() failure");
+        return -1;
+    }
+    // ループバックデバイスを作成・登録します。
+    dev = loopback_init();
+    if (!dev) {
+        errorf("loopback_init() failure");
+        return -1;
+    }
+    // プロトコルスタックを起動します。
+    if (net_run() == -1) {
+        errorf("net_run() failure");
+        return -1;
+    }
+    // キーボードからの割り込みシグナルを受信するまで繰り返します。
+    while (!terminate) {
+        // ループバックデバイスを利用してテストデータを送信します。
+        if (net_device_output(dev, NET_PROTOCOL_TYPE_IP, test_data, sizeof(test_data), NULL) == -1) {
+            errorf("net_device_output() failure");
+            break;
+        }
+        // 1秒スリープします。
+        sleep(1);
+    }
+    // プロトコルスタックを停止します。
+    net_shutdown();
+    return 0;
+}
