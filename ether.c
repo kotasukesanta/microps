9c13ce62cc4668d73f396b11e1d822f56c26f9b6
Hiroshi Matsumoto <5595351+kotasukesanta@users.noreply.github.com>
2023-09-03 11:38:01 +0900

    step12

--- ether.c
+++ ether.c
@@ -9,6 +9,7 @@
 #include "net.h"
 #include "ether.h"
 
+// Ethernetヘッダ構造体
 struct ether_hdr {
     uint8_t dst[ETHER_ADDR_LEN];
     uint8_t src[ETHER_ADDR_LEN];
@@ -18,6 +19,7 @@ struct ether_hdr {
 const uint8_t ETHER_ADDR_ANY[ETHER_ADDR_LEN] = {"\x00\x00\x00\x00\x00\x00"};
 const uint8_t ETHER_ADDR_BROADCAST[ETHER_ADDR_LEN] = {"\xff\xff\xff\xff\xff\xff"};
 
+// 文字列のハードウェアアドレスを変換します。
 int
 ether_addr_pton(const char *p, uint8_t *n)
 {
@@ -42,6 +44,7 @@ ether_addr_pton(const char *p, uint8_t *n)
     return 0;
 }
 
+// ハードウェアアドレスを文字列に変換します。
 char *
 ether_addr_ntop(const uint8_t *n, char *p, size_t size) {
     if (!n || !p) {
@@ -51,17 +54,81 @@ ether_addr_ntop(const uint8_t *n, char *p, size_t size) {
     return p;
 }
 
+// Ethernetフレームをダンプします。
 static void
 ether_dump(const uint8_t *frame, size_t flen)
 {
+    struct ether_hdr *hdr;
+    char addr[ETHER_ADDR_STR_LEN];
+
+    hdr = (struct ether_hdr *)frame;
+    flockfile(stderr);
+    fprintf(stderr, "        src: %s\n", ether_addr_ntop(hdr->src, addr, sizeof(addr)));
+    fprintf(stderr, "        dst: %s\n", ether_addr_ntop(hdr->dst, addr, sizeof(addr)));
+    fprintf(stderr, "       type: 0x%04x\n", ntoh16(hdr->type));
+#ifdef HEXDUMP
+    hexdump(stderr, frame, flen);
+#endif
+    funlockfile(stderr);
 }
 
+// Ethernetフレームを送信します。
+int
+ether_transmit_helper(struct net_device *dev, uint16_t type, const uint8_t *data, size_t len, const void *dst, ether_transmit_func_t callback)
+{
+    uint8_t frame[ETHER_FRAME_SIZE_MAX] = {};
+    struct ether_hdr *hdr;
+    size_t flen, pad = 0;
+
+    hdr = (struct ether_hdr *)frame;
+    memcpy(hdr->dst, dst, ETHER_ADDR_LEN);
+    memcpy(hdr->src, dev->addr, ETHER_ADDR_LEN);
+    hdr->type = hton16(type);
+    memcpy(hdr+1, data, len);
+    if (len < ETHER_PAYLOAD_SIZE_MIN) {
+        pad = ETHER_PAYLOAD_SIZE_MIN - len;
+    }
+    flen = sizeof(*hdr) + len + pad;
+    debugf("dev=%s, type=0x%04x, len=%zu", dev->name, type, flen);
+    ether_dump(frame, flen);
+    return callback(dev, frame, flen) == (ssize_t)flen ? 0 : -1;
+}
+
+// Ethernetフレームを受領します。
 int
 ether_input_helper(struct net_device *dev, ether_input_func_t callback)
 {
+    uint8_t frame[ETHER_FRAME_SIZE_MAX];
+    ssize_t flen;
+    struct ether_hdr *hdr;
+    uint16_t type;
+
+    flen = callback(dev, frame, sizeof(frame));
+    if (flen < (ssize_t)sizeof(*hdr)) {
+        errorf("too short");
+        return -1;
+    }
+    hdr = (struct ether_hdr *)frame;
+    if (memcmp(dev->addr, hdr->dst, ETHER_ADDR_LEN) != 0) {
+        if (memcmp(ETHER_ADDR_BROADCAST, hdr->dst, ETHER_ADDR_LEN) != 0) {
+            /* for other host */
+            return -1;
+        }
+    }
+    type = ntoh16(hdr->type);
+    debugf("dev=%s, type=0x%04x, len=%zd", dev->name, type, flen);
+    ether_dump(frame, flen);
+    return net_input_handler(type, (uint8_t *)(hdr+1), flen - sizeof(*hdr), dev);
 }
 
+// Ethernetデバイスを設定します。
 void
 ether_setup_helper(struct net_device *dev)
 {
+    dev->type = NET_DEVICE_TYPE_ETHERNET;
+    dev->mtu = ETHER_PAYLOAD_SIZE_MAX;
+    dev->flags = (NET_DEVICE_FLAG_BROADCAST | NET_DEVICE_FLAG_NEED_ARP);
+    dev->hlen = ETHER_HDR_SIZE;
+    dev->alen = ETHER_ADDR_LEN;
+    memcpy(dev->broadcast, ETHER_ADDR_BROADCAST, ETHER_ADDR_LEN);
 }
